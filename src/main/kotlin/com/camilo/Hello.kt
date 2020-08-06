31fdeebdfc980c35238987ad9a1f3a7701ac28d1
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2020-08-05 21:55:03 -0300

    Primeiro passos com apache kafka

---
+++ src/main/kotlin/com/camilo/Hello.kt
@@ -0,0 +1,6 @@
+package com.camilo
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/main/kotlin/com/camilo/NewOrderMain.kt
@@ -0,0 +1,38 @@
+package com.camilo
+
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import org.apache.kafka.common.serialization.StringSerializer
+import java.lang.Exception
+import java.util.*
+import java.util.Objects.*
+
+class NewOrderMain {}
+
+fun main(args: Array<String>) {
+    val producer = KafkaProducer<String, String>(properties())
+    val value = "12312321,123123,1231"
+    val record = ProducerRecord("ECOMMERCE_NEW_ORDER", value, value);
+    producer.send(record, handlerMessage()).get()
+}
+
+private fun handlerMessage(): (RecordMetadata, Exception) -> Unit {
+    return { data, ex ->
+        if (nonNull(ex)) {
+            ex.printStackTrace()
+        } else {
+            println("Sucesso enviando: ${data.topic()} :: partition ${data.partition()} / offset ${data.offset()} /timestamp ${data.timestamp()}")
+        }
+
+    }
+}
+
+private fun properties(): Properties {
+    val properties = Properties()
+    properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
+    properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+    properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+    return properties
+}
