057983515651eaf5aeed4968b9595164cc96dfe1
Luis Camilo <lhenriquegomescamilo@hotmail.com>
2020-08-23 01:06:03 -0300

    Added configuration that provider confirmation when delivery the messages

--- common-kafka/src/main/kotlin/com/camilo/KafkaDispatcher.kt
+++ common-kafka/src/main/kotlin/com/camilo/KafkaDispatcher.kt
@@ -19,6 +19,7 @@ class KafkaDispatcher<T> : Closeable {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9090")
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GsonSerializer::class.java.name)
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all")
         return properties
     }
 
